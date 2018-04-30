2b16a800c8982f1e6df85bb7f87e5a485bac5f5c
Marek Goldmann <mgoldman@redhat.com>
2018-04-30 18:23:34 +0200

    Import of ARQ tests

---
+++ tests/arq/README.md
@@ -0,0 +1,42 @@
+# Arquillian Tests for Red Hat Single-Sign On (SSO)
+
+This project contains OpenShift v3 tests for the Red Hat SSO/Keycloak image and the SSO/Keycloak capabilities of the EAP image
+
+## Requirements
+
+The following are required to run the tests:
+
+* Maven 3+
+* OpenShift environment
+* Available `oc` command
+* You should use DNS server that is able to resolve domains used/create by OpenShift environment
+
+## Usage
+
+Before you can execute tests you need to gather some required information, which is:
+
+* User token used to authenticate against OpenShift
+* Router IP is the IP of the hosts where OpenShift router is deployed
+* OpenShift URL is the hostname and port combination of the API endpoint
+* Route suffix is the domain name that will be used and
+
+
+```
+# Log in into OpenShift
+oc login OPENSHIFT_URL:OPENSHIFT_PORT -u OPENSHIFT_USER -p OPENSHIFT_PASSWORD
+
+# Retrieve token
+TOKEN=$(oc whoami -t)
+```
+
+Now you can run the tests
+
+```
+mvn clean install -Dkubernetes.master=https://${OPENSHIFT_URL}:${OPENSHIFT_PORT} -Dkubernetes.auth.token=${TOKEN} -Drouter.hostIP=${ROUTER_IP} -Droute.suffix=${ROUTE_SUFFIX} -Darquillian.startup.timeout=6000
+```
+
+Additionally you can (or even should) specify which images should be used to run the tests against by specifying additional parameters:
+
+* `-DimageStream.sso72.image` - full name of the SSO 7.2 image
+* `-DimageStream.eap71.image` - full name of the EAP 7.1 image
+* `-DimageStream.eap64.image` - full name of the EAP 6.4 image
---
+++ tests/arq/src/test/java/org/jboss/test/arquillian/ce/sso/SsoTestBase.java
@@ -0,0 +1,52 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.arquillian.ce.sso;
+
+import org.arquillian.cube.openshift.api.OpenShiftDynamicImageStreamResource;
+import org.arquillian.cube.openshift.api.OpenShiftResource;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.URL;
+import java.util.logging.Logger;
+
+
+/**
+ * @author Filippe Spolti
+ * @author Ales justin
+ */
+@OpenShiftResource("${openshift.imageStreams}")
+@OpenShiftDynamicImageStreamResource(name = "${imageStream.sso72.name:redhat-sso72-openshift}", image = "${imageStream.sso72.image:registry.access.redhat.com/redhat-sso-7/sso72-openshift:1.1}", version = "${imageStream.sso72.version:1.1}")
+public abstract class SsoTestBase {
+    protected final Logger log = Logger.getLogger(getClass().getName());
+
+    protected abstract URL getRouteURL();
+
+    protected abstract URL getSecureRouteURL();
+
+    public static void assertContains(String content, String... expecteds) {
+        for (String expected : expecteds)
+            assertTrue(String.format("String does not contain the expected value \"%s\". Content: %s", expected, content), content.contains(expected));
+    }
+}
