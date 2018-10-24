b710a4b63e2c55513e1ea978034973bf549c8ba6
Douglas Palmer <dpalmer@redhat.com>
2018-10-24 12:34:35 -0700

    Fix template urls

--- tests/arq/src/test/java/org/jboss/test/arquillian/ce/sso/Sso72ServerPostgresqlTest.java
+++ tests/arq/src/test/java/org/jboss/test/arquillian/ce/sso/Sso72ServerPostgresqlTest.java
@@ -33,7 +33,7 @@ import org.junit.runner.RunWith;
 import java.net.URL;
 
 @RunWith(Arquillian.class)
-@Template(url = "https://raw.githubusercontent.com/${template.repository:jboss-openshift}/application-templates/${template.branch:master}/sso/sso72-postgresql.json",
+@Template(url = "file://${user.dir}/templates/sso-cd-postgresql.json",
         labels = "application=sso,component=server",
         parameters = {
                 @TemplateParameter(name = "IMAGE_STREAM_NAMESPACE", value = "${kubernetes.namespace:openshift}"),
