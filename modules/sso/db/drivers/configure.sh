781ddea585d8167dfaaa24110e49b8cb8dc060cb
Hynek Mlnarik <hmlnarik@redhat.com>
2020-03-19 21:40:46 +0100

    KEYCLOAK-13357 Upgrade MariaDB driver

--- modules/sso/db/drivers/configure.sh
+++ modules/sso/db/drivers/configure.sh
@@ -10,9 +10,7 @@ function link {
   ln -s $1 $2
 }
 
-link /usr/lib/java/mariadb-java-client.jar $JBOSS_HOME/modules/system/layers/openshift/org/mariadb/main/mariadb-java-client.jar
 link /usr/share/java/postgresql-jdbc.jar $JBOSS_HOME/modules/system/layers/openshift/org/postgresql/main/postgresql-jdbc.jar
 
-# module definitions for MariaDB and PostgreSQL
 # Remove any existing destination files first (which might be symlinks)
 cp -rp --remove-destination "$ADDED_DIR/modules" "$JBOSS_HOME/"
