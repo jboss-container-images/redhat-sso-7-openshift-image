70b045d02dcde67a400c10c78b3f91407c49141e
Denis Richtarik <drichtar@redhat.com>
2021-10-14 10:18:29 +0200

    CIAM-1035 java-11-openj9-devel installed despite of IBM Semeru 11 being specified in OpenJ9 build + remove Java 8

--- modules/sso/sso-rm-openjdk/11/configure.sh
+++ modules/sso/sso-rm-openjdk/11/configure.sh
@@ -2,19 +2,8 @@
 set -u
 set -e
 
-## Work around https://bugzilla.redhat.com/show_bug.cgi?id=1797054
-if rpm -q java-1.8.0-openjdk-headless && ( rpm -q java-11-openjdk-devel || rpm -q java-11-openj9-devel ); then
-    for pkg in java-1.8.0-openjdk-devel \
-           java-1.8.0-openjdk-headless \
-           java-1.8.0-openjdk; do
-        if rpm -q "$pkg"; then
-            rpm -e --nodeps "$pkg"
-        fi
-    done
-fi
-
 ## Work around OpenJDK being installed as dependency. https://bugzilla.redhat.com/show_bug.cgi?id=1762827 and similar
-if rpm -q java-11-openj9-devel; then
+if rpm -q ibm-semeru-open-11-jdk || rpm -q java-11-openj9-devel; then
     for pkg in java-11-openjdk-devel \
            java-11-openjdk-headless \
            java-11-openjdk; do
