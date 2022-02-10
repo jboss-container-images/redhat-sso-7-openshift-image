21bad64614cf469907365b0fc75e9b2978827174
Denis Richtarik <drichtar@redhat.com>
2022-02-10 16:41:51 +0100

    CIAM-2174 Merge container builds to one multiarch build and use OpenJDK for all architectures

--- modules/sso/sso-jdk/11/configure.sh
+++ modules/sso/sso-jdk/11/configure.sh
@@ -17,23 +17,15 @@ pushd ${ARTIFACTS_DIR}
 cp -pr * /
 popd
 
-# Set this JDK as the alternative in use
-if [[ ($(uname -i) = ppc64le) || ($(uname -i) = s390x) ]]
-then
-  alternatives --set java /usr/lib/jvm/ibm-semeru-open-11-jdk/bin/java
-  alternatives --set javac /usr/lib/jvm/ibm-semeru-open-11-jdk/bin/javac
-  JAVA_SECURITY_FILE=/usr/lib/jvm/ibm-semeru-open-11-jdk/conf/security/java.security
-  JAVA_HOME=/usr/lib/jvm/ibm-semeru-open-11-jdk/
-else
-  _arch="$(uname -i)"
-  alternatives --set java java-11-openjdk.${_arch}
-  alternatives --set javac java-11-openjdk.${_arch}
-  alternatives --set java_sdk_openjdk java-11-openjdk.${_arch}
-  alternatives --set jre_openjdk java-11-openjdk.${_arch}
-  JAVA_SECURITY_FILE=/usr/lib/jvm/java/conf/security/java.security
-  JAVA_HOME=/usr/lib/jvm/java-11/
-fi
+_arch="$(uname -i)"
+alternatives --set java java-11-openjdk.${_arch}
+alternatives --set javac java-11-openjdk.${_arch}
+alternatives --set java_sdk_openjdk java-11-openjdk.${_arch}
+alternatives --set jre_openjdk java-11-openjdk.${_arch}
 
+# Update securerandom.source for quicker starts (must be done after removing jdk 8, or it will hit the wrong files)
+JAVA_SECURITY_FILE=/usr/lib/jvm/java/conf/security/java.security
+JAVA_HOME=/usr/lib/jvm/java-11/
 # Update securerandom.source for quicker starts (must be done after removing jdk 11, or it will hit the wrong files)
 SECURERANDOM=securerandom.source
 if grep -q "^$SECURERANDOM=.*" $JAVA_SECURITY_FILE; then
