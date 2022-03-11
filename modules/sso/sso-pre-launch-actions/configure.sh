91cf80fdc8a2565c6d8e0066131ece6ee3552594
Denis Richtarik <drichtar@redhat.com>
2022-03-11 12:07:46 +0100

    CIAM-2404 Ensure using the latest versions of RPM packages by calling microdnf update

--- modules/sso/sso-pre-launch-actions/configure.sh
+++ modules/sso/sso-pre-launch-actions/configure.sh
@@ -5,5 +5,8 @@ set -eu
 # shellcheck disable=SC1091
 source "${JBOSS_HOME}/bin/launch/sso-rcfile-definitions.sh"
 
+# CIAM-2404 Ensure using the latest versions of RPM packages by calling microdnf update
+microdnf update -y
+
 # CIAM-1757: On each arch remove JDK 1.8 rpms if present (since using JDK 11 already)
 rpm --query --all name=java* version=1.8.0* | xargs rpm -e --nodeps
