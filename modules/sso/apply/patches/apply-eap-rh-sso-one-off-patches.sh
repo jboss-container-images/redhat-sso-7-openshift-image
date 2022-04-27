c25edb990b3c775e749ecc37fc82eff5c8d3a504
Denis Richtarik <drichtar@redhat.com>
2022-04-27 11:14:12 +0200

    CIAM-2879 Container patch script should accept a dash in the rh-sso patch name

--- modules/sso/apply/patches/apply-eap-rh-sso-one-off-patches.sh
+++ modules/sso/apply/patches/apply-eap-rh-sso-one-off-patches.sh
@@ -6,7 +6,7 @@ readonly SOURCES_DIR="/tmp/artifacts/"
 export JAVA_OPTS="${JAVA_OPTS} -Dorg.wildfly.patching.jar.invalidation=true"
 
 # Do not use for cycle, it would faile if no such files are found
-find "${SOURCES_DIR}" \( -name 'eap-one-off-*.zip' -o -name 'rh-sso-*.zip' \) | while read -r I; do
+find "${SOURCES_DIR}" \( -name 'eap-one-off-*.zip' -o -name 'rh-sso-*.zip' -o -name 'rhsso-*.zip' \) | while read -r I; do
     echo "Applying patch: '$I' ..."
     # CIAM-1975 Prevent any possible 'Conflicts detected:' error while applying
     # the patch by using the '--override-all' option of patch apply command
